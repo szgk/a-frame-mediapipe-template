2a33abc5cb671755caa72c57502f129dae56adee
szgk123@gmail.com <szgk123@gmail.com>
2022-05-05 13:14:36 +0900

    fix: config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+import { defineConfig } from "vite"
+
+export default defineConfig(({ command, mode }) => ({
   root: 'src',
+  base: (mode === 'production')
+  ? '/a-frame-mediapipe-template/' : './',
   build: {
     outDir: '../docs',
     emptyOutDir: true,
   },
-}
+}))
